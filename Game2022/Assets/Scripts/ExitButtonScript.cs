ba0160d352bbcde6e1db0a53ba11e27c6c3196a5
kskrevival <java.enjoyer@gmail.com>
2022-04-06 08:27:53 +0500

    Add ExitButton Script

---
+++ Game2022/Assets/Scripts/ExitButtonScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonScript : MonoBehaviour
+{
+    void OnClick()
+    {
+        Application.Quit();
+    }
+}
