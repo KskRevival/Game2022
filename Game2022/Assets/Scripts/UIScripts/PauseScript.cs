d2a2ae1533e2c626d9786d76499e5822168d1ee6
kskrevival <java.enjoyer@gmail.com>
2022-04-08 09:17:29 +0500

    Added shitty pause script and pause menu, will change it later

--- Game2022/Assets/Scripts/UIScripts/PauseScript.cs
+++ Game2022/Assets/Scripts/UIScripts/PauseScript.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class PauseScript : MonoBehaviour
+public class PauseScript: MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float timer;
+    public bool isPaused;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Time.timeScale = timer;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+        timer = isPaused? 0 : 1f;
+    }
+
+    public void OnGUI()
+    {
+        if (isPaused == true)
+        {
+            Cursor.visible = true;
+            if (GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2) - 150f, 150f, 45f), "Продолжить"))
+            {
+                isPaused = false;
+                timer = 0;
+                Cursor.visible = false;
+            }
+            if (GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2) - 100f, 150f, 45f), "Сохранить"))
+            {
+
+            }
+            if (GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2) - 50f, 150f, 45f), "Загрузить"))
+            {
+
+            }
+            if (GUI.Button(new Rect((float)(Screen.width / 2), (float)(Screen.height / 2), 150f, 45f), "В Меню"))
+            {
+                isPaused = false;
+                timer = 0;
+                SceneManager.LoadScene(0);
+
+            }
+        }
     }
 }
