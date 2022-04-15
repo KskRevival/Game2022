de7722d28af5d940c689ad7a388faf4bc1a8ec99
kskrevival <java.enjoyer@gmail.com>
2022-04-15 11:29:51 +0500

    InfniteLoad, hz chto delat)

--- Game2022/Assets/Scripts/Fight Scripts/FightResults.cs
+++ Game2022/Assets/Scripts/Fight Scripts/FightResults.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FightResults : MonoBehaviour
 {
     public void FightWin()
     {
         ExitScript.ToGame();
+        while (!SceneManager.GetSceneByBuildIndex(2).isLoaded) { }
         Destroy(GameObject.Find("Monster"));
         SaveAndLoad.LoadGame();
     }
