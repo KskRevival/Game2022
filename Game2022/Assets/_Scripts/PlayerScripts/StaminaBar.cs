17fc1f8c0a857eb7f36170b080acad5b9117fe17
kskrevival <java.enjoyer@gmail.com>
2022-05-21 18:06:02 +0500

    merge bug fix

--- Game2022/Assets/_Scripts/PlayerScripts/StaminaBar.cs
+++ Game2022/Assets/_Scripts/PlayerScripts/StaminaBar.cs
@@ -1,3 +1,4 @@
+using System;
 using PlayerScripts;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ public class StaminaBar : MonoBehaviour
 {
     [SerializeField] private Image staminaBarFilling;
     [SerializeField] private Image staminaBar;
+    private const double Tolerance = 1e-10;
 
     private Color blueColor = new Color(0.04939478f, 0.1323116f, 0.7075472f);
     private Color redColor = new Color(0.7058824f, 0.0509804f, 0.1377635f);
@@ -17,28 +19,26 @@ public class StaminaBar : MonoBehaviour
     private void Start()
     {
         staminaBarFilling.color = new Color(blueColor.r,
-                                            blueColor.g,
-                                            blueColor.b, 
-                                            0f);
+            blueColor.g,
+            blueColor.b,
+            0f);
 
         staminaBar.color = new Color(staminaBar.color.r,
-                                     staminaBar.color.g,
-                                     staminaBar.color.b,
-                                     0f);
+            staminaBar.color.g,
+            staminaBar.color.b,
+            0f);
     }
 
     private void Update()
     {
-        Debug.Log(isFaded);
-
         staminaBarFilling.fillAmount = Stamina.stamina;
 
         if (!isFaded) staminaBarFilling.color = Stamina.canRun ? blueColor : redColor;
 
-        if (!isFaded && Stamina.stamina == 1f) StartCoroutine(FadeTo(0f));
-        else if (isFaded && Stamina.stamina != 1f) StartCoroutine(FadeTo(1f));
+        if (!isFaded && Math.Abs(Stamina.stamina - 1f) < Tolerance) StartCoroutine(FadeTo(0f));
+        else if (isFaded && Math.Abs(Stamina.stamina - 1f) > Tolerance) StartCoroutine(FadeTo(1f));
 
-        isFaded = Stamina.stamina == 1f;
+        isFaded = Math.Abs(Stamina.stamina - 1f) < Tolerance;
     }
 
     IEnumerator FadeTo(float alpha)
@@ -48,26 +48,26 @@ public class StaminaBar : MonoBehaviour
             var alphaValue = alpha == 0f ? 1 - i : i;
 
             staminaBarFilling.color = new Color(staminaBarFilling.color.r,
-                                                staminaBarFilling.color.g,
-                                                staminaBarFilling.color.b,
-                                                alphaValue);
+                staminaBarFilling.color.g,
+                staminaBarFilling.color.b,
+                alphaValue);
 
             staminaBar.color = new Color(staminaBar.color.r,
-                                     staminaBar.color.g,
-                                     staminaBar.color.b,
-                                     alphaValue);
+                staminaBar.color.g,
+                staminaBar.color.b,
+                alphaValue);
 
             yield return null;
         }
 
         staminaBarFilling.color = new Color(staminaBarFilling.color.r,
-                                                staminaBarFilling.color.g,
-                                                staminaBarFilling.color.b,
-                                                alpha);
+            staminaBarFilling.color.g,
+            staminaBarFilling.color.b,
+            alpha);
 
         staminaBar.color = new Color(staminaBar.color.r,
-                                 staminaBar.color.g,
-                                 staminaBar.color.b,
-                                 alpha);
+            staminaBar.color.g,
+            staminaBar.color.b,
+            alpha);
     }
 }
