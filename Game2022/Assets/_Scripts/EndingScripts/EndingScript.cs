27844f47db34d9f243bf9c10b83d59fe6ba3dc11
kskrevival <java.enjoyer@gmail.com>
2022-06-03 12:01:47 +0500

    Screen size update

--- Game2022/Assets/_Scripts/EndingScripts/EndingScript.cs
+++ Game2022/Assets/_Scripts/EndingScripts/EndingScript.cs
@@ -14,7 +14,7 @@ public class EndingScript : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
             Smiler.SetActive(true);
 
@@ -24,17 +24,17 @@ public class EndingScript : MonoBehaviour
 
     IEnumerator StartEnding()
     {
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.4f);
 
         Ending.SetActive(true);
         Title.SetActive(true);
 
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(3f);
 
         Title.SetActive(false);
         Authors.SetActive(true);
 
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(3f);
 
         UIScripts.TransitionScript.ToMenu();
     }
